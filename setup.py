a42484585aa9ef42a2962b6f8a2e46ac7ff7a3af
ClaraBuettner <clarabuettner@aol.com>
2021-01-22 10:22:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name='disaggregator',
+    packages= ['disaggregator'],
+    include_package_data=True,
+    install_requires=['numpy',
+                      'pandas',
+                      'tables',                      
+                      'pyyaml',
+                      'requests',
+                      'geopandas',
+                      'descartes',
+                      'xarray',
+                      'xlrd',
+                      'matplotlib',
+                      'holidays'],
+    package_data={
+        "": ["*.csv", "*.xlsx"]}
+)
